c37464c955e82ff9c91b83fb8bbd741fbc563c73
MrDerman <60555628+MrDerman@users.noreply.github.com>
2020-05-04 20:05:14 +0100

    Add files via upload

--- Stars/sketch.js
+++ Stars/sketch.js
@@ -1,56 +1,26 @@
 var nodes = [];
-var minNodes = 30;
+var colours = [];
+var minNodes = 35;
 var tSize = 150;
 var speed = 2;
-var c1; c2; c3; c4; c5; c6; c7;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   minNodes = round((floor((width + height / 2) / 100)/23) * minNodes);
-  console.log(minNodes);
-  c1 = color(249,153,186); //pink
-  c2 = color(166,216,241); //light blue
-  c3 = color(165,224,229); //blue
-  c4 = color(224,173,203); //purple
-  c5 = color(192,199,223); //purple2
-  c6 = color(222,217,145); //yellow
+  colours.push(color(249,153,186),color(166,216,241),color(165,224,229),color(222,217,145),color(224,173,203),color(192,199,223));
 
   for (var i = 0; i < minNodes; i++) {
-    r = floor(random(1,8));
+    r = floor(random(0,6));
     pos = createVector(random(0,windowWidth), random (0,windowHeight));
-    if(r==1){
-      nodes.push(new node(tSize,speed,c1, pos));
-    }else if (r==2){
-      nodes.push(new node(tSize,speed,c2, pos));
-    }else if (r==3){
-      nodes.push(new node(tSize,speed,c3, pos));
-    }else if (r==4){
-      nodes.push(new node(tSize,speed,c4, pos));
-    }else if (r==5){
-      nodes.push(new node(tSize,speed,c5, pos));
-    }else if (r==6) {
-      nodes.push(new node(tSize,speed,c6, pos));
-    }
+    nodes.push(new node(tSize,speed,colours[r], pos));
   }
 }
 
 function draw() {
   background(255,255,255);
   if(nodes.length < minNodes) {
-    r = floor(random(1,7))
-    if(r==1) {
-      nodes.push(new node(tSize,speed,c1));
-    }else if (r==2) {
-      nodes.push(new node(tSize,speed,c2));
-    }else if (r==3) {
-      nodes.push(new node(tSize,speed,c3));
-    }else if (r==4) {
-      nodes.push(new node(tSize,speed,c4));
-    }else if (r==5) {
-      nodes.push(new node(tSize,speed,c5));
-    }else if (r==6) {
-      nodes.push(new node(tSize,speed,c6));
-    }
+    r = floor(random(0,6));
+    nodes.push(new node(tSize,speed,colours[r]));
   }
   
   for (var i = 0; i < nodes.length; i++) {
@@ -60,5 +30,4 @@ function draw() {
     nodes[i].render();
     nodes[i].move();
   }
-
 }
